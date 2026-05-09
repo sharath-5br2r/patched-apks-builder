cca8bbaa76f2b5464a2ff1bee650cda0ed62f067
Sharath <sharathsukash@gmail.com>
2026-05-09 14:16:48 +0530

    Fix conditional check for apkpure in build.sh

--- src/build/build.sh
+++ src/build/build.sh
@@ -3,8 +3,7 @@ source src/build/utils.sh
 dl_gh "morphe-cli" "MorpheApp" "latest"
 dl_gh $1 $2 "prerelease"
 get_patches_key $4
-echo $6
-if [ $6 == "apkpure" ]
+if [[ $6 == "apkpure" ]]
 then
   get_apk $3 $4 $5 $7
 else
