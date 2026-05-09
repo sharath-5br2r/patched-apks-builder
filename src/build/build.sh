cae2b98100cc024b4a07328a33f1015623810064
Sharath <sharathsukash@gmail.com>
2026-05-09 14:01:05 +0530

    Update build.sh to conditionally call get_apk
    Refactor get_apk call to handle 'apkpure' method conditionally.

--- src/build/build.sh
+++ src/build/build.sh
@@ -1,8 +1,13 @@
 source src/build/utils.sh
-# repo owner pkgname appname regex method
+# repo owner pkgname appname regex method/apkpure method(apkpure)
 dl_gh "morphe-cli" "MorpheApp" "latest"
 dl_gh $1 $2 "prerelease"
 get_patches_key $4
-get_apk $3 $4 $4 $5 $6
+if [ $6 == "apkpure" ]
+then
+  get_apk $3 $4 $5 $7
+else
+  get_apk $3 $4 $4 $5 $6
+fi
 split_editor $4 $4
 patch $4 $2 "morphe"
