eaf06401665150da918293484ee04a6c15fe98e6
Sharath <sharathsukash@gmail.com>
2026-05-09 22:17:10 +0530

    Update build.sh

--- src/build/build.sh
+++ src/build/build.sh
@@ -3,14 +3,20 @@ source src/build/utils.sh
 dl_gh "morphe-cli" "MorpheApp" "latest"
 dl_gh $1 $2 "prerelease"
 get_patches_key $4
+if [[ $3 == "com.amazon.avod.thirdpartyclient" ]]
+then
+ version1=$(printf '%s\n' "$version" "$prefer_version" | sort -V | tail -n1)
+ version=${version1%.*}
+fi 
 if [[ $6 == "apkpure" ]]
 then
   get_apkpure $3 $4 $5 $7
-elif [[ $3 == "com.amazon.avod.thirdpartyclient" ]]
-then
-  get_apk "com.amazon.avod.thirdpartyclient" "prime-video-arm64-v8a" "amazon-prime-video" "amazon-mobile-llc/amazon-prime-video/amazon-prime-video" "arm64-v8a"
 else
   get_apk $3 $4 $4 $5 $6
 fi
+if [[ $3 == "com.amazon.avod.thirdpartyclient" ]]
+then
+
+fi 
 split_editor $4 $4
 patch $4 $2 "morphe"
