dfadc4f3e5451a96b64a0106edeaa5469d2f462f
SharathGames1 <sharathsukash@gmail.com>
2026-05-14 20:36:49 +0530

    Fix syntax

--- src/etc/check_eden.sh
+++ src/etc/check_eden.sh
@@ -3,9 +3,9 @@
 # Check new Eden Version:
 
 checker(){
-	local date1 date2 date1_sec date1_sec ur_repo=$repository tag=
+	local date1 date2 date1_sec date1_sec ur_repo=$repository
 	date1=$(gh run list -R Eden-CI/Workflow -w nightly.yml --status success --limit 1 --json updatedAt  -q ".[0].updatedAt")
-	date2=$(gh api repos/$ur_repo/releases |jq -r 'first(.[] | select(.tag_name == "'Eden PUBG'") | .assets[] | .updated_at )')
+	date2=$(gh api repos/$ur_repo/releases |jq -r 'first(.[] | select(.tag_name == "'Eden-PUBG'") | .assets[] | .updated_at )')
 	date1_sec=$(date -d "$date1" +%s)
 	date2_sec=$(date -d "$date2" +%s)
 	if [ -z "$date2" ] || [ "$date1_sec" -gt "$date2_sec" ]; then
