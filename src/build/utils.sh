2018576921301cf5cfa35b8a8edc7b674373986c
Sharath <sharathsukash@gmail.com>
2026-06-18 15:50:11 +0530

    fix syntax

--- src/build/utils.sh
+++ src/build/utils.sh
@@ -748,7 +748,7 @@ patch() {
 		if [[ "$3" = inotia || "$3" = morphe ]]; then
 			unset CI GITHUB_ACTION GITHUB_ACTIONS GITHUB_ACTOR GITHUB_ENV GITHUB_EVENT_NAME GITHUB_EVENT_PATH GITHUB_HEAD_REF GITHUB_JOB GITHUB_REF GITHUB_REPOSITORY GITHUB_RUN_ID GITHUB_RUN_NUMBER GITHUB_SHA GITHUB_WORKFLOW GITHUB_WORKSPACE RUN_ID RUN_NUMBER
 		fi
-		if [[ $1 == "repatch"]]; then
+		if [[ $1 == "repatch" ]]; then
 			mv ./release/$file_name ./downlaod/
 			eval java -jar *cli*.jar $p$b  --keystore-password=$KEYSTORE_PASS --keystore-entry-password=$KEYSTORE_PASS --keystore-entry-alias=$KEYSTORE_ALIAS --keystore=ks.keystore $m$opt --out=./release/${file_name%.*}-$2-$release_name.apk$excludePatches$includePatches$ks $pu$force $a./download/$1.apk
 		else
