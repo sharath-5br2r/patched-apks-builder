3438080059f1c737a724530f8ea277cde3cf36ad
Sharath <sharathsukash@gmail.com>
2026-06-23 10:43:31 +0530

    Small changes

--- src/build/utils_mod.sh
+++ src/build/utils_mod.sh
@@ -80,7 +80,8 @@ dl_gh_v2(){
 
 # Modifed version of dl_gl to include tag and custom domain
 dl_gl_mod() {
-  local repo=$1 tag=${2:-latest} output=$3 domain=$4
+  tag=$2
+  local repo=$1  output=$3 domain=$4
   if [ -z "$domain" ]; then
     domain="gitlab.com"
   fi
@@ -201,7 +202,8 @@ patch_mod() {
 		unset lock_version
 		unset options
 		if [[ $makemodule == "true" ]]; then
-			code=$(gh api /repos/sharath-5br2r/patched-apks-builder/releases/tags/$2-$3 | jq -r '.assets[]? | select(.name == "update-$4.json") | .url' | xargs wget -qO- | jq -r '.versionCode // 0') || yes
+		    repotag="$apppname$pname"
+			code=$(gh api /repos/$github_repo/releases/tags/$repotag | jq -r '.assets[]? | select(.name == "update-$arch.json") | .url' | xargs wget -qO- | jq -r '.versionCode // 0') || yes
 			if [ -z "$code" ] ; then
 				code=1
 			else
@@ -219,11 +221,11 @@ patch_mod() {
 				archname=$arch
 			fi
 			echo -e "PKG_NAME=$pkgname\nPKG_VER=$version-p$patchversion\nMODULE_ARCH=$archname" > ./module/config
-			echo -e "id=$n\nname=$name_in$pname\nversion=$version (patches $pname - $)\nversionCode=$code\nauthor=sharath-5br2r\ndescription=$2 $3 Module\nupdateJson=https://github.com/sharath-5br2r/patched-apks-builder/releases/tag/$2-$3/update-$4.json" > ./module/module.prop
+			echo -e "id=$n\nname=$name_in$pname\nversion=$version (patches $pname - $pversion)\nversionCode=$code\nauthor=sharath-5br2r\ndescription=$appname $pname Module\nupdateJson=https://github.com/sharath-5br2r/patched-apks-builder/releases/tag/$repotag/update-$arch.json" > ./module/module.prop
 			zip -r "./release/$name_out.zip" ./module/ > /dev/null 2>&1
 			green_log "[+] Module created: ./release/$name_out.zip"
 			rm -rf ./module ./release/$name_out.apk ./rv_module
-			echo -e "{\n\"version\":\"$version\",\n\"versionCode\":$code,\n\"zipUrl\":\"https://github.com/sharath-5br2r/patched-apks-builder/releases/download/$2-$3/$2-$3-$version-p$patchversion.zip\"\n}" > ./release/update-$4.json
+			echo -e "{\n\"version\":\"$version\",\n\"versionCode\":$code,\n\"zipUrl\":\"https://github.com/$github_repo/releases/download/$repotag/$name_out.zip\"\n}" > ./release/update-$arch.json
 
 		fi
 	else
